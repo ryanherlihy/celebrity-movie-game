466191d640d8ffdcbdfd6980da025a9a49670370
Ryan Herlihy <ryanherlihy@Ryans-Air.fios-router.home>
2019-10-18 12:05:36 -0400

    create get function for tmdb api, implement movie search

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,38 @@
 const express = require("express");
+const https = require("https");
 
 const PORT = 3000;
+
+const TMDB_API_BASE_PATH = "https://api.themoviedb.org/3";
+const TMDB_API_KEY = "07befa3ecc55734111b95a62335771fb";
+const TMDB_IMAGE_BATH_PATH = "https://image.tmdb.org/t/p/w200/";
+
+const tmdbApiGet = ({ endpoint, queryFields }) => {
+  const query = Object.entries(queryFields)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
+
+  return new Promise((resolve, reject) => {
+    https
+      .get(
+        `${TMDB_API_BASE_PATH}${endpoint}?api_key=${TMDB_API_KEY}&${query}`,
+        apiRes => {
+          let data = "";
+
+          apiRes.on("data", chunk => {
+            data += chunk;
+          });
+
+          apiRes.on("end", () => {
+            resolve(JSON.parse(data));
+          });
+        }
+      )
+      .on("error", err => {
+        reject(err);
+      });
+  });
+};
 
 const app = express();
 
@@ -16,7 +48,31 @@ app.get("/", (req, res) => {
  * Input: search string
  * Output: list of movies with titles matching input
  */
-app.get("/getMovies", (req, res) => {});
+app.get("/getMovies", (req, res) => {
+  const search = req.query.search;
+
+  if (search) {
+    tmdbApiGet({
+      endpoint: "/search/movie",
+      queryFields: {
+        query: search
+      }
+    })
+      .then(({ results }) => {
+        res.send(
+          results.map(({ title, poster_path, release_date }) => ({
+            title,
+            imageUrl: `${TMDB_IMAGE_BATH_PATH}${poster_path}`,
+            releaseYear: new Date(release_date).getFullYear()
+          }))
+        );
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send("There was a problem retrieving search results.");
+      });
+  }
+});
 
 /**
  * Endpoint to return list of actors for user to guess from
