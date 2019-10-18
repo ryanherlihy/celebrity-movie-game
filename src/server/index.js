377dd34239fd13012615a4fc547e4ec579d454ee
Ryan Herlihy <ryanherlihy@Ryans-Air.fios-router.home>
2019-10-17 20:01:43 -0400

    add endpoints

--- src/server/index.js
+++ src/server/index.js
@@ -10,4 +10,28 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
+/**
+ * Endpoint to return list of movies for user to search from
+ *
+ * Input: search string
+ * Output: list of movies with titles matching input
+ */
+app.get("/getMovies", (req, res) => {});
+
+/**
+ * Endpoint to return list of actors for user to guess from
+ *
+ * Input: movie id
+ * Output: list of actors
+ */
+app.get("/getActors", (req, res) => {});
+
+/**
+ * Endpoint for user to submit a guess
+ *
+ * Input: movie id, list of guessed actor ids, list of all actor ids
+ * Output: movie id, list of all correct actor ids, user score
+ */
+app.post("/submitGuess", (req, res) => {});
+
 app.listen(PORT, () => console.log(`App listening on port ${PORT}...`));
